f24b8a5d3016c7a33e5a855e256688d97a30181e
Abdallah Kawji <contact@abdallah.dev>
2026-04-08 23:33:45 +0200

    Update formula to v1.0.19

--- Formula/tidal-sync.rb
+++ Formula/tidal-sync.rb
@@ -1,8 +1,8 @@
 class TidalSync < Formula
   desc "Tidal playlist sync CLI — Master quality"
   homepage "https://github.com/14h/tidal-sync"
-  url "https://github.com/14h/tidal-sync/archive/refs/tags/v1.0.18.tar.gz"
-  sha256 "90441a7fff717b77e1f4b6e95d107b2f73ba87bd00cfd94c0f384b6299a79c2d"
+  url "https://github.com/14h/tidal-sync/archive/refs/tags/v1.0.19.tar.gz"
+  sha256 "0556363be5b24c8f98d5acd260695dacda1998540427b52be88d249690b90100"
   license "Apache-2.0"
 
   depends_on "node"
